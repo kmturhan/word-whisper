4bd35c1ef15627c8b8d945247114adf7433507bd
kmturhan <kmturhan@gmail.com>
2023-11-26 05:48:09 +0300

    update

--- WordWhisper.Web/Program.cs
+++ WordWhisper.Web/Program.cs
@@ -12,7 +12,9 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
+app.Logger.LogInformation("START PROJECT");
+var movieApiKey = builder.Configuration["ConnectionStrings:SqlServer"];
+app.Logger.LogInformation(movieApiKey);
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
