fcb1b3a33c9780693437e2a54fd834d2cb26257d
kmturhan <kmturhan@gmail.com>
2023-12-09 22:15:16 +0300

    jwt token added

--- WordWhisper.Web/Models/AppSetting.cs
+++ WordWhisper.Web/Models/AppSetting.cs
@@ -3,5 +3,8 @@
     public static class AppSetting
     {
         public static string ConnectionString { get; set; }
+        public static string JwtIssuer { get; set; }
+        public static string JwtAudience { get; set; }
+        public static string JwtSigninKey { get; set; }
     }
 }
