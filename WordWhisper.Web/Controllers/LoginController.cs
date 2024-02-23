7673641f5a6cd9d91813a3338d9a9d66d699ae89
kmturhan <kmturhan@gmail.com>
2024-02-24 02:31:08 +0300

    login page added

---
+++ WordWhisper.Web/Controllers/LoginController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WordWhisper.Web.Controllers
+{
+    
+    public class LoginController : Controller
+    {
+        [Route("login")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
