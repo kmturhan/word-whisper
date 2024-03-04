28a0fc4f9d2ee1027643983e519b32fe7716b4a3
kmturhan <kmturhan@gmail.com>
2024-03-04 03:37:14 +0300

    dto added

---
+++ WordWhisper.Entities/Dtos/Requests/RegisterRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordWhisper.Entities.Dtos.Requests
+{
+    public class RegisterRequest
+    {
+        public string MyProperty { get; set; }
+    }
+}
--- WordWhisper.Web/wwwroot/js/site.js
+++ WordWhisper.Web/wwwroot/js/site.js
@@ -2,3 +2,17 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+var inputField = document.getElementById('username');
+
+inputField.addEventListener('focus', function () {
+    inputField.setAttribute('placeholder', '');
+    alert("focus test");
+});
+
+inputField.addEventListener('blur', function () {
+    if (inputField.value === '') {
+        inputField.setAttribute('placeholder', 'Adınız');
+        alert("blur test");
+
+    }
+});
